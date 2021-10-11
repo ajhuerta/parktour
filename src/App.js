87388cc5b6cf97cd6fb1fe49f28b4a34beecb654
Anthony Huerta <ajhuerta6@gmail.com>
2021-10-10 22:28:14 -0500

    changes to landing page
    Added new packages as well as implemented a very basic version of landing page.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root:{
+    height: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/flatten.jpg'})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: 'cover',
+  },
+}));
+export default function () {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <p>
-          :o
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = {classes.root}>
+     <CssBaseline />
+     <Header/>
     </div>
   );
 }
-
-export default App;
